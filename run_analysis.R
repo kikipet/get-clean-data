812582ed4fbc9066b2dd8da1b28a92c5642e43b2
kikipet <songkimwl@gmail.com>
2020-08-06 18:46:08 -0400

    commented run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -9,28 +9,41 @@
 # Appropriately labels the data set with descriptive variable names.
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-wd <- "/home/skim52/coursera/get-clean-data/UCI HAR Dataset/"
+# set wd to where the UCI HAR Dataset is located
+wd <- "/home/skim52/coursera/get-clean-data/get-clean-data/UCI HAR Dataset/"
+# training data
 train <- read.table(paste(wd, "train/X_train.txt", sep=""))
+# testing data
 test <- read.table(paste(wd, "test/X_test.txt", sep=""))
+# merge training and testing data
 merged <- rbind(train, test)
+# these are where mean and std are located
 keep <- c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202,
           214:215, 227:228, 240:241, 253:254, 266:271, 345:350,
           424:429, 503:504, 516:517, 529:530, 542:543)
 meansd <- merged[, keep]
+# label the dataset
 namedf <- read.table(paste(wd, "features.txt", sep=""))
 names(meansd) <- namedf[keep, 2]
 
+# label for activity ID
 trainL <- read.table(paste(wd, "train/y_train.txt", sep=""))
 testL <- read.table(paste(wd, "test/y_test.txt", sep=""))
 mergeL <- rbind(trainL, testL)
 names(mergeL) <- "Activity"
+
+# label for subject ID
 trainS <- read.table(paste(wd, "train/subject_train.txt", sep=""))
 testS <- read.table(paste(wd, "test/subject_test.txt", sep=""))
 mergeS <- rbind(trainS, testS)
 names(mergeS) <- "Subject"
 
+# combine data for mean/SD with activity and subject labels
 merged2 <- cbind(meansd, mergeL, mergeS)
+# give the activities better labels
 merged2$Activity <- ordered(merged2$Activity, levels = c(1,2,3,4,5,6), labels = c("Walking", "Walking.Upstairs", "Walking.Downstairs", "Sitting", "Standing", "Laying"))
+
+# create separate data frames to use for exporting data files later
 activity <- data.frame()
 for (v in 1:66) {
   tmp <- data.frame(tapply(merged2[,v], merged2$Activity, mean))
@@ -43,6 +56,7 @@ for (v in 1:66) {
   }
 }
 row.names(activity) <- c("Walking", "Walking.Upstairs", "Walking.Downstairs", "Sitting", "Standing", "Laying")
+
 subject <- data.frame()
 for (v in 1:66) {
   tmp <- data.frame(tapply(merged2[,v], merged2$Subject, mean))
@@ -54,6 +68,8 @@ for (v in 1:66) {
     subject <- cbind(subject, tmp)
   }
 }
+
+# write txt files with data
 write.table(merged2, file = paste(wd, "../mean-sd.txt", sep=""), sep = " ", row.names = FALSE, col.names = TRUE)
 write.table(activity, file = paste(wd, "../mean_activity.txt", sep=""), sep = " ", row.names = TRUE, col.names = TRUE)
 write.table(subject, file = paste(wd, "../mean_subject.txt", sep=""), sep = " ", row.names = FALSE, col.names = TRUE)
